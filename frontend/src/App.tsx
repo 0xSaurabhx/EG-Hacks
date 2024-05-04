5caaabec1f6a64000a8d47366ef36f98e067589c
Royal <royal.21ai040@sode-edu.in>
2024-05-04 11:16:15 +0530

    add vite,tailwindcss,shadcn to the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css'
-
+import "./App.css"
+import { Button } from "@/components/ui/button"
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline bgcolor">
       Hello world!
+      <Button>Click me</Button>
     </h1>
   )
 }

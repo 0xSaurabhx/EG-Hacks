1d127761e3db37a66357c0f8cedc33f696370111
Royal <royal.21ai040@sode-edu.in>
2024-05-04 10:44:13 +0530

    add vite, tailwindcss to frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bgcolor">
+      Hello world!
+    </h1>
+  )
+}
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,11 @@
-import React from 'react'
-import './Header.css'
 
 
- return (
-    <div className='navbar'>
-       
-     <ul>
-    <li><Link to='/'> Home</Link> </li>
-    <li><Link to='/coins' > About</Link> </li>
-     </ul>
+const Header = () => {
+  return (
+    <div>
+      
     </div>
   )
-
+}
 
 export default Header

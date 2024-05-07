a9d259f2f5bdd5dcf4830f37ad75e473185f8438
0xSaurabhx <masterinindia11@gmail.com>
2024-05-07 19:46:15 +0530

    Added /convert Route to Convert the Code

--- backend/app.py
+++ backend/app.py
@@ -4,13 +4,26 @@ import psycopg2
 from psycopg2 import pool
 from flask import Flask, request
 from flask_cors import CORS
+from groq import Groq
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
 DATABASE_URL = "postgres://default:xr4Mlmbi8RzI@ep-autumn-sea-a1loat7s.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 connection_pool = pool.SimpleConnectionPool(1, 10, DATABASE_URL, sslmode='require')
+
+client = Groq(
+    api_key=os.environ["MISTRAL_API_KEY"],
+)
+
+ALLOWED_EXTENSIONS = {'pas', 'dfm', 'cob', 'cbl', 'vb', 'vbs'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/", methods=["GET"])
 def home():
@@ -63,6 +76,35 @@ def signin():
 
     connection_pool.putconn(conn)
     return {"status": 200, "email": email, "message": "Signed in successfully"}, 200
+
+
+@app.route("/convert", methods=["POST"])
+def convert():
+    if 'file' not in request.files:
+        return {"status": 400, "error": "No file part"}, 400
+    
+    file = request.files['file']
+    if file.filename == '':
+        return {"status": 400, "error": "No selected file"}, 400
+
+    if file and allowed_file(file.filename):
+        code = file.read().decode('utf-8')  
+        from_code = request.args.get('from')
+        to_code = request.args.get('to')
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"""{code}
+                    covert the above {from_code} program to {to_code}
+                    """,
+                }
+            ],
+            model="mixtral-8x7b-32768",
+        )
+        return chat_completion.choices[0].message.content
+
+    return {"status": 400, "error": "Invalid file extension"}, 400
 
 
 if __name__ == '__main__':

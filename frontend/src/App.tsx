6a716320c37d6639493baf6a7566d44539535e15
0xSaurabhx <masterinindia11@gmail.com>
2024-05-15 00:31:54 +0530

    Deploy Ready

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import "./styles/App.css";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Signin from "./pages/Signin";
 import Contact from "./pages/Contact";
@@ -14,23 +14,22 @@ import Optimize from "./pages/Optimizee";
 import Docs from "./pages/Docs";
 export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/chatbot" element={<Chatbot />} />
-          <Route path="/codegen" element={<Codegen />} />
-          <Route path="/convert/:id" element={<ConversionHistory />} />
-       
-          <Route path="/debug" element={<Debug/>} />
-          <Route path="/optimize" element={<Optimize />} />
-          <Route path="/docs/:id" element={<Docs />} />
-        </Routes>
-      </BrowserRouter>
-      <Notification /> 
+    <><Router>
+      <Routes>
+        <Route path="/" exact element={<LandingPage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/codegen" element={<Codegen />} />
+        <Route path="/convert/:id" element={<ConversionHistory />} />
+
+        <Route path="/debug" element={<Debug />} />
+        <Route path="/optimize" element={<Optimize />} />
+        <Route path="/docs/:id" element={<Docs />} />
+      </Routes>
+      <Notification />
+    </Router>
     </>
   );
 }

73f16f462574745d71539e9bc2f0124e95e701f5
Royal <royal.21ai040@sode-edu.in>
2024-06-26 23:27:04 +0530

    feat: replace r2 with S3

--- backend/app.py
+++ backend/app.py
@@ -7,8 +7,6 @@ import requests
 import psycopg2
 import textwrap
 
-
-
 from groq import Groq
 from io import BytesIO
 from psycopg2 import pool
@@ -18,60 +16,56 @@ from flask import Flask, request, jsonify
 from botocore.client import Config
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
 load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-#CONFIG
+# CONFIG
 BUCKET = "demo"
 ALLOWED_EXTENSIONS = {"pas", "dfm", "cob", "cbl", "vb", "vbs"}
-DATABASE_URL = "postgres://default:xr4Mlmbi8RzI@ep-autumn-sea-a1loat7s.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require"
-ACCOUNTID = os.environ["ACCOUNTID"]
-CLIENTACCESSKEY = os.environ["CLIENTACCESSKEY"]
-CLIENTSECRET = os.environ["CLIENTSECRET"]
-CONNECTIONURL = f"https://{ACCOUNTID}.r2.cloudflarestorage.com"
-
+DATABASE_URL = os.getenv("DATABASE_URL")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
 
 conn = psycopg2.connect(DATABASE_URL, sslmode="require")
-connection_pool = pool.SimpleConnectionPool(
-    1, 10, DATABASE_URL, sslmode="require")
+connection_pool = pool.SimpleConnectionPool(1, 10, DATABASE_URL, sslmode="require")
 client = Groq(
     api_key=os.environ["MISTRAL_API_KEY"],
 )
 
+s3_client = boto3.client(
+    "s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_DEFAULT_REGION,
+)
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def upload_to_r2(filename, filecontent):
-    s3 = boto3.client(
-    service_name ='s3',
-    endpoint_url=CONNECTIONURL,
-    aws_access_key_id=CLIENTACCESSKEY,
-    aws_secret_access_key=CLIENTSECRET,
-    region_name='us-east-1'
-)
-    UploadObject = filecontent
-    UploadObjectBytes = UploadObject.encode('utf-8')
-    UploadObjectFile = BytesIO(UploadObjectBytes)
+def upload_to_s3(filename, filecontent):
     try:
-        s3.upload_fileobj(UploadObjectFile, Bucket="demo", Key=f"{filename}.json")
+        s3_client.put_object(
+            Bucket=S3_BUCKET_NAME,
+            Key=f"{filename}.json",
+            Body=filecontent.encode("utf-8"),
+            ContentType="application/json",
+        )
         return True
-    except:
+    except Exception as e:
+        print(f"Upload error: {e}")
         return False
-
 
 @app.route("/", methods=["GET"])
 def home():
     con_id = str(uuid.uuid4())
-    return "Running on Python with Postgres ", 200
-
+    return "Running on Python with Postgres", 200
 
 @app.route("/signup", methods=["POST"])
 def signup():
     data = request.get_json()
-
     email = data.get("email")
     password = data.get("password")
 
@@ -90,8 +84,12 @@ def signup():
     conn.commit()
     connection_pool.putconn(conn)
 
-    return {"status": 200, "email": email, "id": user_id, "message": "User created successfully"}, 200
-
+    return {
+        "status": 200,
+        "email": email,
+        "id": user_id,
+        "message": "User created successfully",
+    }, 200
 
 @app.route("/signin", methods=["POST"])
 def signin():
@@ -117,8 +115,12 @@ def signin():
             return {"status": 400, "error": "Invalid password"}, 200
     connection_pool.putconn(conn)
 
-    return {"status": 200, "email": email, "id": user_id, "message": "Signed in successfully"}, 200
-
+    return {
+        "status": 200,
+        "email": email,
+        "id": user_id,
+        "message": "Signed in successfully",
+    }, 200
 
 @app.route("/convert", methods=["POST"])
 def convert():
@@ -144,11 +146,11 @@ def convert():
                 {
                     "role": "user",
                     "content": f"""{code}
-                    covert the above {from_code} program to {to_code}, give the code first and then documentations
+                    convert the above {from_code} program to {to_code}, give the code first and then documentations
                     """,
                 }
             ],
-            model="llama3-70b-8192",
+            model="llama3-8b-8192",
         )
 
         con_id = str(uuid.uuid4())
@@ -160,26 +162,34 @@ def convert():
             )
         conn.commit()
         connection_pool.putconn(conn)
+
         content = chat_completion.choices[0].message.content
+        print("Chat Completion Content:", content)  # Debugging line
+
         code_match = re.search(r"```(.*?)```", content, re.DOTALL)
-        codee = code_match.group(1).strip()
-        lines = codee.split('\n')
-        lines = lines[1:]
-        codee = '\n'.join(lines)
-        if upload_to_r2(filename=f"{con_id}-old",filecontent=code) and upload_to_r2(filename=f"{con_id}-new",filecontent=codee):
-            return {"code":codee,"id":con_id, "title": con_title}, 200
-        else:
-            return {"status": 400, "error": "Invalid file extension"}, 400
+        if code_match:
+            codee = code_match.group(1).strip()
+            lines = codee.split("\n")
+            lines = lines[1:]
+            codee = "\n".join(lines)
+            if upload_to_s3(
+                filename=f"{con_id}-old", filecontent=code
+            ) and upload_to_s3(filename=f"{con_id}-new", filecontent=codee):
+                return {"code": codee, "id": con_id, "title": con_title}, 200
+            else:
+                return {"status": 400, "error": "File upload failed"}, 400
 
     return {"status": 400, "error": "Invalid file extension"}, 400
 
 @app.route("/chatid/get", methods=["GET"])
 def get_chat_ids():
-    user_id = request.args.get('userid')
+    user_id = request.args.get("userid")
     conn = connection_pool.getconn()
     try:
         with conn.cursor() as cur:
-            cur.execute("SELECT con_id,title FROM history WHERE user_id = %s", (user_id,))
+            cur.execute(
+                "SELECT con_id,title FROM history WHERE user_id = %s", (user_id,)
+            )
             records = cur.fetchall()
         conn.commit()
     except Exception as e:
@@ -190,56 +200,33 @@ def get_chat_ids():
 
 @app.route("/docs/get", methods=["GET"])
 def get_docs():
-    chatid = request.args.get('chatid')
-    title = request.args.get('title')
-    chat_url = f"https://pub-ed6294b09052471093b13f036a7fe802.r2.dev/{chatid}.json"
+    chatid = request.args.get("chatid")
+    title = request.args.get("title")
+    chat_url = f"https://s3.{AWS_DEFAULT_REGION}.amazonaws.com/{S3_BUCKET_NAME}/{chatid}.json"
     response = requests.get(chat_url)
+    if response.status_code != 200:
+        return {"status": 400, "error": "Failed to fetch the code from S3"}, 400
+
     code = response.text
-    prompt = prompt = f"""
+    prompt = f"""
 You are given a code snippet below:
 
 {code}
 
 The above code is in "{title}".
 
-Please generate a detailed html document for this code that includes the following sections and format, Use Arial & sans-serif font family throught the html document:
+Please generate a detailed html document for this code that includes the following sections and format, Use Arial & sans-serif font family throughout the html document:
 [Documentation for {title} ({code})]
-1. Project Overview: Provide a step by step explaination of what this code does.
+1. Project Overview: Provide a step by step explanation of what this code does.
 2. Setup Instructions: Include instructions on how to set up the project. This should cover installation steps, dependencies, and configuration required to run the code.
 3. Usage Instructions: Detail how to use the code. Include examples of commands or functions, and describe what the expected outputs are.
 4. Examples: Provide some example usages of the code along with expected outcomes.
 5. Error Handling: Describe any error handling implemented in the code or any common issues a user might encounter and how to resolve them.
 
-Make sure the html file is well-structured and easy to follow, using appropriate html syntax for headers, code blocks, and linkss. Use Arial, sans-serif fonat family throught the html document.
+Make sure the html file is well-structured and easy to follow, using appropriate html syntax for headers, code blocks, and links. Use Arial, sans-serif font family throughout the html document.
 """
 
     chat_completion = client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt,
-                }
-            ],
-            model="mixtral-8x7b-32768",
-        )
-    content = chat_completion.choices[0].message.content
-    return content
-
-
-
-@app.route("/debug/get", methods=["GET"])
-def debug_code():
-    chatid = request.args.get('chatid')
-    title = request.args.get('title')
-    chat_url = f"https://pub-ed6294b09052471093b13f036a7fe802.r2.dev/{chatid}.json"
-    response = requests.get(chat_url)
-    code = response.text
-    prompt = prompt = f"""{code}
-    above code is coverted {title}, debug the code if neccessary and add comments regarding the changes you made. Also add propper heading and subheading
-    """
-
-    try:
-        chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "user",
@@ -248,10 +235,37 @@ def debug_code():
         ],
         model="llama3-8b-8192",
     )
+    content = chat_completion.choices[0].message.content
+    return content
+
+@app.route("/debug/get", methods=["GET"])
+def debug_code():
+    chatid = request.args.get("chatid")
+    title = request.args.get("title")
+    chat_url = f"https://s3.{AWS_DEFAULT_REGION}.amazonaws.com/{S3_BUCKET_NAME}/{chatid}.json"
+    response = requests.get(chat_url)
+    if response.status_code != 200:
+        return {"status": 400, "error": "Failed to fetch the code from S3"}, 400
+
+    code = response.text
+    prompt = f"""{code}
+    above code is converted {title}, debug the code if necessary and add comments regarding the changes you made. Also add proper heading and subheading
+    """
+
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            model="llama3-8b-8192",
+        )
     except Exception as e:
         return f"An error occurred: {e} --- {prompt}"
     content = chat_completion.choices[0].message.content
     return content
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True, port=8181)
